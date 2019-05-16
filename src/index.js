2c84572cf457d8e98c623e6a6cc9aaf3aa18e4c1
Jerome Simeon <jeromesimeon@me.com>
2019-05-16 15:07:27 -0400

    (feature) props to say whether to show Edit button
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/index.js
+++ src/index.js
@@ -512,13 +512,14 @@ function MarkdownEditor(props) {
 
   return (
     <div>
-      <Segment raised>
-        <Checkbox toggle label='Edit' onChange={toggleShowSlate} checked={props.markdownMode ? !showSlate : showSlate} />
-      </Segment>
-    <Card.Group>
-      {card}
-  </Card.Group>
-  </div>
+      { props.showEditButton ?
+        <Segment raised>
+          <Checkbox toggle label='Edit' onChange={toggleShowSlate} checked={props.markdownMode ? !showSlate : showSlate} />
+        </Segment> : null }
+      <Card.Group>
+        {card}
+      </Card.Group>
+    </div>
   );
 }
 
@@ -550,6 +551,11 @@ MarkdownEditor.propTypes = {
   lockText: PropTypes.bool,
 
   /**
+   * If true then show the edit button.
+   */
+  showEditButton: PropTypes.bool,
+
+  /**
    * An array of plugins to extend the functionality of the editor
    */
   plugins: PropTypes.arrayOf(PropTypes.shape({
@@ -567,5 +573,11 @@ MarkdownEditor.propTypes = {
     schema: PropTypes.object.isRequired,
   })),
 };
+/**
+ * The default property values for this component
+ */
+MarkdownEditor.defaultProps = {
+  showEditButton: true,
+}
 
 export { MarkdownEditor };
