01c3498ac7f5406fc4d9f5643b1b15d9bd5d34ed
irmerk <jolenelanglinais@gmail.com>
2019-05-22 17:38:43 -0400

    feat(icons): move styling into svg component - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- public/icons/bold.js
+++ public/icons/bold.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const BoldIcon = fillColor => (
+export const type = () => 'bold';
+
+export const height = () => '23px';
+
+export const width = () => '25px';
+
+export const padding = () => '5px 7px';
+
+export const vBox = () => '0 0 11 13';
+
+export const icon = fillColor => (
     <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="IBMPlexSans-Bold, IBM Plex Sans" fontSize="18" fontWeight="bold">
         <g id="contract---clause-edit-within-contract-editor" transform="translate(-440.000000, -48.000000)" fill={fillColor}>
             <text id="B-Copy">
@@ -9,5 +19,3 @@ const BoldIcon = fillColor => (
         </g>
     </g>
 );
-
-export default BoldIcon;
