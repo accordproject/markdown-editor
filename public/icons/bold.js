6d32b100cc375e34a6e9632af24788fc6bcdf77d
irmerk <jolenelanglinais@gmail.com>
2019-05-22 16:48:37 -0400

    feat(icons): implement bold svg as react component - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ public/icons/bold.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BoldIcon = fillColor => (
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="IBMPlexSans-Bold, IBM Plex Sans" fontSize="18" fontWeight="bold">
+        <g id="contract---clause-edit-within-contract-editor" transform="translate(-440.000000, -48.000000)" fill={fillColor}>
+            <text id="B-Copy">
+                <tspan x="439" y="61">B</tspan>
+            </text>
+        </g>
+    </g>
+);
+
+export default BoldIcon;
