61059d83f5c3879f8261b7b7043e7f845a2d76c2
irmerk <jolenelanglinais@gmail.com>
2019-05-23 10:26:33 -0400

    feat(icons): move param svg to react component - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ public/icons/param.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const type = () => 'bold';
+
+export const height = () => '24px';
+
+export const width = () => '25px';
+
+export const padding = () => '6px 3px';
+
+export const vBox = () => '0 0 19 12';
+
+const x = '{x}';
+
+export const icon = fillColor => (
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="ArialRoundedMTBold, Arial Rounded MT Bold" fontSize="11.5" fontWeight="normal" letterSpacing="-0.819999993">
+        <g id="contract---clause-edit-within-contract-editor" transform="translate(-652.000000, -49.000000)" fill={fillColor}>
+            <text id="[{x}]">
+                <tspan x="652" y="58">[{x}</tspan>
+                <tspan x="667.632109" y="58">]</tspan>
+            </text>
+        </g>
+    </g>
+);
