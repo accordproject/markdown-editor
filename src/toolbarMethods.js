5ba286cc8d74b7b72d80ad4839648d02dbd2b845
irmerk <jolenelanglinais@gmail.com>
2019-05-31 11:40:26 -0400

    feat(src): modularize methods - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/toolbarMethods.js
@@ -0,0 +1,143 @@
+/**
+ * A change helper to standardize wrapping links.
+ */
+export const wrapLink = (editor, href) => {
+  editor.wrapInline({
+    type: 'link',
+    data: { href },
+  });
+  editor.moveToEnd();
+};
+
+/**
+   * A change helper to standardize unwrapping links.
+   */
+export const unwrapLink = (editor) => {
+  editor.unwrapInline('link');
+};
+
+/**
+   * Check if the current selection has a mark with `type` in it.
+   */
+export const hasMark = (editor, type) => {
+  const { value } = editor;
+  return value.activeMarks.some(mark => mark.type === type);
+};
+
+/**
+   * Check whether the current selection has a link in it.
+   */
+export const hasLinks = (editor) => {
+  const { value } = editor;
+  return value.inlines.some(inline => inline.type === 'link');
+};
+
+/**
+  * Return selected block of given type.
+  */
+export const getSelectedBlock = (editor, type) => {
+  const { value } = editor;
+  return value.blocks.find(node => node.type === type);
+};
+
+/**
+  * Return selected block of 'list_item' type.
+  */
+const getSelectedListBlock = (editor) => {
+  const { value } = editor;
+  return value.blocks.find(node => node.type === 'list_item');
+};
+
+/**
+  * Return whether current selection is a list.
+  */
+export const getListBool = (editor, type) => {
+  const { value } = editor;
+  const selectedBlockHere = getSelectedListBlock(editor);
+  return selectedBlockHere
+    ? (selectedBlockHere.type === 'list_item')
+    : (value.blocks.some(node => node.type === type));
+};
+
+/**
+  * Return whether current selection is a given type.
+  */
+export const getTypeBool = (editor, type) => {
+  const { value } = editor;
+  const { document } = value;
+  return value.blocks
+    .some(block => !!document.getClosest(block.key, parent => parent.type === type));
+};
+
+/**
+  * Return parent block of given selection.
+  */
+const getParentBlock = (editor, type, item) => {
+  const { value } = editor;
+  const { document } = value;
+  return document.getClosest(item.key, parent => parent.type === type);
+};
+
+/**
+  * Return type of given selected parent.
+  */
+const getParentBlockType = block => (block ? block.type : block);
+
+/**
+  * Check if the any of the currently selected blocks are of `type`.
+  */
+export const hasBlock = (editor, type) => {
+  const { value } = editor;
+  const { document } = value;
+  const selectListItem = getSelectedBlock(editor, 'list_item');
+  // If selection is a list, return the parent type.
+  if (selectListItem) {
+    const parentBlock = getParentBlock(editor, type, selectListItem);
+    const parentBlockType = getParentBlockType(parentBlock);
+    return value
+      .blocks.some(block => !!document
+        .getClosest(block.key, parent => parent.type === parentBlockType));
+  }
+  // If selection is a not a list, return the type.
+  return value.blocks.some(node => node.type === type);
+};
+
+/**
+   * When clicking a link, if the selection has a link in it, remove the link.
+   * Otherwise, add a new link with an href and text.
+   */
+export const onClickLink = (event, editor) => {
+  event.preventDefault();
+
+  const { value } = editor;
+  const hasLinksBool = hasLinks(editor);
+
+  if (hasLinksBool) {
+    editor.command(unwrapLink);
+  } else if (value.selection.isExpanded) {
+    const href = window.prompt('Enter the URL of the link:');
+
+    if (href === null) {
+      return;
+    }
+
+    editor.command(wrapLink, href);
+  } else {
+    const href = window.prompt('Enter the URL of the link:');
+
+    if (href === null) {
+      return;
+    }
+
+    const text = window.prompt('Enter the text for the link:');
+
+    if (text === null) {
+      return;
+    }
+
+    editor
+      .insertText(text)
+      .moveFocusBackward(text.length)
+      .command(wrapLink, href);
+  }
+};
