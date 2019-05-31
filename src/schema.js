b9eec6cf9987f90dbc47d0742f95831a31932057
irmerk <jolenelanglinais@gmail.com>
2019-05-31 11:39:15 -0400

    feat(schema): add code to list marks - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/schema.js
+++ src/schema.js
@@ -58,7 +58,7 @@ const schema = {
     list_item: {
       parent: [{ type: 'ol_list' }, { type: 'ul_list' }],
       nodes: [{ match: [{ object: 'text' }, { type: 'link' }] }],
-      marks: [{ type: 'bold' }, { type: 'italic' }],
+      marks: [{ type: 'bold' }, { type: 'italic' }, { type: 'code' }],
     }
   },
 };
