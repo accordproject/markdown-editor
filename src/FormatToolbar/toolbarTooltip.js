9b63ae02a7a89c578d8cba92184723b3081096dd
irmerk <jolenelanglinais@gmail.com>
2019-07-02 11:56:35 -0400

    feat(FormatToolbar): merge all additions to new directory - I35
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/FormatToolbar/toolbarTooltip.js
@@ -0,0 +1,32 @@
+/**
+ *************** INTERNAL METHODS ***************
+ */
+
+/**
+ * Methods for conversion to strings and further manipulation.
+ */
+const capitalizeFirst = word => word.toString().charAt(0).toUpperCase();
+
+const sliceWord = word => word.toString().slice(1);
+
+const firstTwoLetters = word => word.toString().slice(0, 2);
+
+/**
+ *************** EXTERNAL METHODS ***************
+ */
+
+/**
+ * Converts an input into a string, capitalizes the first letter, and returns string
+ */
+export const capitalizeWord = word => capitalizeFirst(word) + sliceWord(word);
+
+/**
+ * Checks the beginning of a block type to determine the string to return
+ */
+export const identifyBlock = (block) => {
+  const typeBeginning = firstTwoLetters(block);
+  if (typeBeginning === 'bl') return 'Quote';
+  if (typeBeginning === 'ul') return 'Bulleted List';
+  if (typeBeginning === 'ol') return 'Numbered List';
+  return null;
+};
