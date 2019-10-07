8095420d0c6d60b96e551564a62bc6b940c0778a
Monika <monikahoex@gmail.com>
2019-10-07 16:50:25 -0700

    fix(src/icons/*): remove id attribute from svg icons - #109
    Signed-off-by: Monika Hoex <monikahoex@gmail.com>
    Signed-off-by: Monika <monikahoex@gmail.com>

--- src/icons/bold.js
+++ src/icons/bold.js
@@ -11,11 +11,21 @@ export const padding = () => '5px 7px';
 export const vBox = () => '0 0 11 13';
 
 export const icon = fillColor => (
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="IBMPlexSans-Bold, IBM Plex Sans" fontSize="18" fontWeight="bold">
-        <g id="contract---clause-edit-within-contract-editor" transform="translate(-440.000000, -48.000000)" fill={fillColor}>
-            <text id="B-Copy">
-                <tspan x="439" y="61">B</tspan>
-            </text>
-        </g>
+  <g
+    stroke="none"
+    strokeWidth="1"
+    fill="none"
+    fillRule="evenodd"
+    fontFamily="IBMPlexSans-Bold, IBM Plex Sans"
+    fontSize="18"
+    fontWeight="bold"
+  >
+    <g transform="translate(-440.000000, -48.000000)" fill={fillColor}>
+      <text>
+        <tspan x="439" y="61">
+          B
+        </tspan>
+      </text>
     </g>
+  </g>
 );
--- src/icons/underline.js
+++ src/icons/underline.js
@@ -11,14 +11,20 @@ export const padding = () => '3px 6px';
 export const vBox = () => '0 0 13 17';
 
 export const icon = fillColor => (
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g id="contract--Formatting-toolbar" transform="translate(-512.000000, -47.000000)" fill={fillColor}>
-            <g id="Underline" transform="translate(512.000000, 42.000000)">
-                <text id="U" fontFamily="IBMPlexSans-Bold, IBM Plex Sans" fontSize="18" fontWeight="bold">
-                    <tspan x="0" y="18">U</tspan>
-                </text>
-                <rect id="Rectangle" x="0" y="20" width="13" height="2"></rect>
-            </g>
-        </g>
+  <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g transform="translate(-512.000000, -47.000000)" fill={fillColor}>
+      <g transform="translate(512.000000, 42.000000)">
+        <text
+          fontFamily="IBMPlexSans-Bold, IBM Plex Sans"
+          fontSize="18"
+          fontWeight="bold"
+        >
+          <tspan x="0" y="18">
+            U
+          </tspan>
+        </text>
+        <rect x="0" y="20" width="13" height="2"></rect>
+      </g>
     </g>
+  </g>
 );
