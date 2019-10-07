84dd5226ea1033e824b0394098da1ee9f8e903b3
Monika <monikahoex@gmail.com>
2019-10-07 16:50:25 -0700

    fix(src/icons/*): remove id attribute from svg icons - #109
    Signed-off-by: Monika Hoex <monikahoex@gmail.com>
    Signed-off-by: Monika <monikahoex@gmail.com>
    Signed-off-by: Dieter <dkbuntzen@gmail.com>

--- src/icons/italic.js
+++ src/icons/italic.js
@@ -11,11 +11,22 @@ export const padding = () => '5px 7px';
 export const vBox = () => '0 0 10 13';
 
 export const icon = fillColor => (
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="IBMPlexSans-BoldItalic, IBM Plex Sans" fontSize="18" fontStyle="italic" fontWeight="bold">
-        <g id="contract---clause-edit-within-contract-editor" transform="translate(-477.000000, -48.000000)" fill={fillColor}>
-            <text id="I">
-                <tspan x="478" y="61">I</tspan>
-            </text>
-        </g>
+  <g
+    stroke="none"
+    strokeWidth="1"
+    fill="none"
+    fillRule="evenodd"
+    fontFamily="IBMPlexSans-BoldItalic, IBM Plex Sans"
+    fontSize="18"
+    fontStyle="italic"
+    fontWeight="bold"
+  >
+    <g transform="translate(-477.000000, -48.000000)" fill={fillColor}>
+      <text>
+        <tspan x="478" y="61">
+          I
+        </tspan>
+      </text>
     </g>
+  </g>
 );
--- src/icons/param.js
+++ src/icons/param.js
@@ -13,12 +13,25 @@ export const vBox = () => '0 0 19 12';
 const x = '{x}';
 
 export const icon = fillColor => (
-    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontFamily="ArialRoundedMTBold, Arial Rounded MT Bold" fontSize="11.5" fontWeight="normal" letterSpacing="-0.819999993">
-        <g id="contract---clause-edit-within-contract-editor" transform="translate(-652.000000, -49.000000)" fill={fillColor}>
-            <text id="[{x}]">
-                <tspan x="652" y="58">[{x}</tspan>
-                <tspan x="667.632109" y="58">]</tspan>
-            </text>
-        </g>
+  <g
+    stroke="none"
+    strokeWidth="1"
+    fill="none"
+    fillRule="evenodd"
+    fontFamily="ArialRoundedMTBold, Arial Rounded MT Bold"
+    fontSize="11.5"
+    fontWeight="normal"
+    letterSpacing="-0.819999993"
+  >
+    <g transform="translate(-652.000000, -49.000000)" fill={fillColor}>
+      <text>
+        <tspan x="652" y="58">
+          [{x}
+        </tspan>
+        <tspan x="667.632109" y="58">
+          ]
+        </tspan>
+      </text>
     </g>
+  </g>
 );
