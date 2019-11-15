dd11c0eedf2f8b2f7d2d0175b6410c7d6b761eb9
irmerk <jolenelanglinais@gmail.com>
2019-11-15 15:47:47 -0500

    refactor(constants): export block types as constants - I192
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const LIST_ITEM = 'list_item';
+export const BLOCK_QUOTE = 'block_quote';
+export const OL_LIST = 'ol_list';
+export const UL_LIST = 'ul_list';
+export const PARAGRAPH = 'paragraph';
