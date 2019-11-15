d2249cc5f22eef5f0d1f1c0680d5450bfb64ee01
irmerk <jolenelanglinais@gmail.com>
2019-11-15 16:25:09 -0500

    docs(FormattingToolbar): explain methods - I192
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/FormattingToolbar/toolbarMethods.js
+++ src/FormattingToolbar/toolbarMethods.js
@@ -5,13 +5,6 @@ import * as CONST from '../constants';
  */
 
 /**
-   * A change helper to standardize unwrapping links.
-   */
-const unwrapLink = (editor) => {
-  editor.unwrapInline('link');
-};
-
-/**
   * Return type of given selected parent.
   */
 const getParentBlockType = block => (block ? block.type : block);
@@ -44,30 +37,36 @@ const wrapLink = (editor, href) => {
   editor.moveToEnd();
 };
 
-const ancestors = value => value.document
-  .getAncestors(value.selection.anchor.path);
+/**
+ * A Slate helper to get ancestors of the selection.
+ */
+const ancestors = val => val.document.getAncestors(val.selection.anchor.path);
 
-const isSelectionOLList = value => ancestors(value)
-  .find(mark => mark.type === CONST.OL_LIST);
+/**
+ * A helper to find boolean of if Slate ancestors includes ol_list.
+ */
+const isSelectionOLList = val => ancestors(val).find(b => b.type === CONST.OL_LIST);
 
-const isSelectionULList = value => ancestors(value)
-  .find(mark => mark.type === CONST.UL_LIST);
+/**
+ * A helper to find boolean of if Slate ancestors includes ul_list.
+ */
+const isSelectionULList = val => ancestors(val).find(b => b.type === CONST.UL_LIST);
 
 /**
  *************** EXPORT METHODS ***************
  */
 
 /**
-   * Check if the current selection has a mark with `type` in it.
-   */
+ * Check if the current selection has a mark with `type` in it.
+ */
 export const hasMark = (editor, type) => {
   const { value } = editor;
   return value.activeMarks.some(mark => mark.type === type);
 };
 
 /**
-   * Check whether the current selection has a link in it.
-   */
+ * Check whether the current selection has a link in it.
+ */
 export const hasLinks = (editor) => {
   const { value } = editor;
   return value.inlines.some(inline => inline.type === 'link');
@@ -122,8 +121,8 @@ export const hasBlock = (editor, type) => {
 };
 
 /**
-   * When clicking apply, update the link with the specified text and href.
-   */
+ * When clicking apply, update the link with the specified text and href.
+ */
 export const applyLinkUpdate = (event, editor) => {
   event.preventDefault();
   const { url: { value: href }, text: { value: text } } = event.target;
@@ -142,13 +141,25 @@ export const applyLinkUpdate = (event, editor) => {
     .command(wrapLink, href);
 };
 
+/**
+ * A helper to find boolean of if Slate ancestors includes list_item.
+ */
 export const isSelectionList = value => ancestors(value).reverse()
   .some(mark => mark.type === CONST.LIST_ITEM);
 
+/**
+ * A helper to find boolean of if input type is a block_quote.
+ */
 export const isClickBlockQuote = input => input === CONST.BLOCK_QUOTE;
 
+/**
+ * A helper to hold the list type of the selection.
+ */
 export const currentList = value => isSelectionOLList(value) || isSelectionULList(value);
 
+/**
+ * A trigger to the Slate editor to make a list_item into a block_quote.
+ */
 export const transformListToBlockQuote = (editor, type, value) => {
   editor.withoutNormalizing(() => {
     editor
@@ -158,10 +169,16 @@ export const transformListToBlockQuote = (editor, type, value) => {
   });
 };
 
+/**
+ * A trigger to the Slate editor to make a paragraph into a block_quote.
+ */
 export const transformParagraphToBlockQuote = (editor, type) => {
   editor.setBlocks(hasBlock(editor, type) ? CONST.PARAGRAPH : type);
 };
 
+/**
+ * A trigger to the Slate editor to make a list_item into a paragraph.
+ */
 export const transformListToParagraph = (editor, type) => {
   editor.withoutNormalizing(() => {
     editor
@@ -171,6 +188,9 @@ export const transformListToParagraph = (editor, type) => {
   });
 };
 
+/**
+ * A trigger to the Slate editor to swap a ul_list to ol_list or vice versa.
+ */
 export const transformListSwap = (editor, type, value) => {
   editor.withoutNormalizing(() => {
     editor
@@ -181,6 +201,9 @@ export const transformListSwap = (editor, type, value) => {
   });
 };
 
+/**
+ * A trigger to the Slate editor to make a block_quote into a list_item.
+ */
 export const transformBlockQuoteToList = (editor, type) => {
   editor
     .setBlocks(CONST.PARAGRAPH)
@@ -188,6 +211,9 @@ export const transformBlockQuoteToList = (editor, type) => {
     .wrapBlock({ type, data: { tight: true } });
 };
 
+/**
+ * A trigger to the Slate editor to make a paragraph into a list_item.
+ */
 export const transformParagraphToList = (editor, type) => {
   editor.withoutNormalizing(() => {
     editor.wrapBlock({ type, data: { tight: true } }).wrapBlock(CONST.LIST_ITEM);
