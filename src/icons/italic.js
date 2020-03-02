0b3861d8ed3a67b52ca0d3b4729954e080123afb
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-03-02 22:31:47 +0530

    style(icons): reverted back the icons to originals - I124
    Signed-off-by: Shrey Sachdeva <shreysachdeva.2000@gmail.com>

--- src/icons/italic.js
+++ src/icons/italic.js
@@ -19,7 +19,8 @@ export const icon = fillColor => (
     fontFamily="IBMPlexSans-BoldItalic, IBM Plex Sans"
     fontSize="16"
     fontStyle="italic"
-  >
+    fontWeight="bold"
+    >
     <g transform="translate(-477.000000, -48.000000)" fill={fillColor}>
       <text>
         <tspan x="478" y="61">
