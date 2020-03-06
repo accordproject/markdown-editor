cb39d1fa2b7c74d7ad6c612d7839bf3339a2c48f
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-03-06 11:40:25 +0530

    Revert "@shrey-sachdeva2000"
    This reverts commit d4d188222bba8cc38f4c9a59bf1b4f94ace7e196.
    
    Signed-off-by: Shrey Sachdeva <shreysachdeva.2000@gmail.com>

--- src/icons/bold.js
+++ src/icons/bold.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import * as tips from '../FormattingToolbar/toolbarTooltip';
 
-export const type = () => `Bold (${tips.MOD}+B)`;
+export const type = () => 'Bold (' + tips.MOD + '+B)';
 
 export const height = () => '25px';
 
