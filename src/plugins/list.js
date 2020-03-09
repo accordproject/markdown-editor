59a4dbff450681421d7c9d60b17617e0059d176a
lenvi <lenvin@oykuapp.com>
2020-03-09 21:40:28 +0530

    implemented un-hack way of nested lists
    Signed-off-by: lenvi <lenvin@oykuapp.com>

--- src/plugins/list.js
+++ src/plugins/list.js
@@ -54,16 +54,10 @@ function List() {
         editor.withoutNormalizing(() => {
         editor.unwrapBlock(CONST.LIST_ITEM)
         editor.wrapBlock({ type: currentList(value).type, data: { tight: true } }) 
+        editor.wrapBlock({ type: currentList(value).type, data: { tight: true } }) 
         editor.wrapBlock(CONST.LIST_ITEM)   
           
       });
-      event.preventDefault();
-      editor.withoutNormalizing(() => {
-      editor.unwrapBlock(CONST.LIST_ITEM)
-      editor.wrapBlock({ type: currentList(value).type, data: { tight: true } }) 
-      editor.wrapBlock(CONST.LIST_ITEM)   
-        
-    });
       return true;
     }
 
