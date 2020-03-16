05bd36b22dd75d6210ec7d59868b5baaf3e9ebe9
Dan Selman <danscode@selman.org>
2020-03-16 02:28:29 +0000

    (chore) clean up schema
    Signed-off-by: Dan Selman <danscode@selman.org>

--- src/RichTextEditor/schema.js
+++ src/RichTextEditor/schema.js
@@ -13,11 +13,21 @@ export const MARK_BOLD = 'bold';
 export const MARK_ITALIC = 'italic';
 export const MARK_CODE = 'code';
 export const MARK_HTML = 'html';
+export const HR = 'horizontal_rule';
+export const LINK = 'link';
+export const IMAGE = 'image';
+export const HTML_BLOCK = 'html_block';
+export const CODE_BLOCK = 'code_block';
+
+export const HTML_INLINE = 'html_inline';
+export const SOFTBREAK = 'softbreak';
+export const LINEBREAK = 'linebreak';
+
 
 export const LIST_TYPES = [OL_LIST, UL_LIST];
 
-export const INLINES = ['linebreak', 'softbreak', 'html_inline', 'image'];
-export const VOIDS = ['linebreak', 'softbreak', 'image'];
+export const INLINES = [LINEBREAK, SOFTBREAK, HTML_INLINE, LINK, IMAGE];
+export const VOIDS = [LINEBREAK, SOFTBREAK, IMAGE, HR];
 export const HEADINGS = [H1, H2, H3, H4, H5, H6];
 
 export const MARKS = [MARK_BOLD, MARK_ITALIC, MARK_CODE, MARK_HTML];
