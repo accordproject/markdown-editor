d29b5197d39960f516349ee6350e3a755c970711
Dan Selman <danscode@selman.org>
2020-03-17 17:26:55 +0000

    (fix) move html paste to use markdown-html
    Signed-off-by: Dan Selman <danscode@selman.org>

--- src/RichTextEditor/components/withHtml.js
+++ src/RichTextEditor/components/withHtml.js
@@ -1,47 +1,8 @@
 import React from 'react';
-import { jsx } from 'slate-hyperscript';
 import { Transforms } from 'slate';
-import * as SCHEMA from '../utilities/schema';
 
-export const deserialize = (el) => {
-  if (el.nodeType === 3) {
-    return el.textContent;
-  } if (el.nodeType !== 1) {
-    return null;
-  } if (el.nodeName === 'BR') {
-    return '\n';
-  }
-
-  const { nodeName } = el;
-  let parent = el;
-
-  if (
-    nodeName === 'PRE'
-    && el.childNodes[0]
-    && el.childNodes[0].nodeName === 'CODE'
-  ) {
-    parent = el.childNodes[0];
-  }
-  const children = Array.from(parent.childNodes)
-    .map(deserialize)
-    .flat();
-
-  if (el.nodeName === 'BODY') {
-    return jsx('fragment', {}, children);
-  }
-
-  if (SCHEMA.HTML_TAGS[nodeName]) {
-    const attrs = SCHEMA.HTML_TAGS[nodeName](el);
-    return jsx('element', attrs, children);
-  }
-
-  if (SCHEMA.HTML_TEXT_TAGS[nodeName]) {
-    const attrs = SCHEMA.HTML_TEXT_TAGS[nodeName](el);
-    return children.map(child => jsx('text', attrs, child));
-  }
-
-  return children;
-};
+const { SlateTransformer } = require('@accordproject/markdown-slate');
+const { HtmlTransformer } = require('@accordproject/markdown-html');
 
 export const withHtml = (editor) => {
   const { insertData } = editor;
@@ -50,9 +11,15 @@ export const withHtml = (editor) => {
     const html = data.getData('text/html');
 
     if (html) {
-      const parsed = new DOMParser().parseFromString(html, 'text/html');
-      const fragment = deserialize(parsed.body);
-      Transforms.insertFragment(editor, fragment);
+      try {
+        const htmlTransformer = new HtmlTransformer();
+        const slateTransformer = new SlateTransformer();
+        const dom = htmlTransformer.toCiceroMark(html);
+        const slate = slateTransformer.fromCiceroMark(dom);
+        Transforms.insertFragment(editor, slate.document.children);
+      } catch (err) {
+        console.log(err);
+      }
       return;
     }
 
