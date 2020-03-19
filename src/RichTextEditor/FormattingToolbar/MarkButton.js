8947929cab4c8d6ee8d8211057a2ebd0359303a8
irmerk <jolenelanglinais@gmail.com>
2020-03-19 16:25:57 -0400

    feat(FormattingToolbar): init mark button - I297
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/RichTextEditor/FormattingToolbar/MarkButton.js
+++ src/RichTextEditor/FormattingToolbar/MarkButton.js
@@ -1,31 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSlate } from 'slate-react';
-import Button from '../components/Button';
-import Icon from '../components/Icon';
-
+import { Popup } from 'semantic-ui-react';
+import { BUTTON_COLORS, POPUP_STYLE } from './StyleConstants';
+import Button2 from '../components/Button2';
 import {
   toggleMark, isMarkActive
 } from '../utilities/toolbarHelpers';
 
-const MarkButton = ({ format, icon }) => {
+const MarkButton = ({
+  type,
+  label,
+  icon,
+  ...props
+}) => {
   const editor = useSlate();
+  const handleMouseDown = (e) => {
+    e.preventDefault();
+    toggleMark(editor, type);
+  };
+  const isActive = isMarkActive(editor, type);
+  const iconColor = isActive
+    ? BUTTON_COLORS.SYMBOL_ACTIVE
+    : BUTTON_COLORS.SYMBOL_INACTIVE;
+  const backgroundColor = isActive
+    ? BUTTON_COLORS.BACKGROUND_ACTIVE
+    : BUTTON_COLORS.BACKGROUND_INACTIVE;
+
   return (
-      <Button
-        active={isMarkActive(editor, format)}
-        onMouseDown={(event) => {
-          event.preventDefault();
-          toggleMark(editor, format);
-        }}
-      >
-        <Icon>{icon}</Icon>
-      </Button>
+    <Popup
+      content={label}
+      style={POPUP_STYLE}
+      position='bottom center'
+      trigger={
+          <Button2
+              aria-label={type}
+              onMouseDown={handleMouseDown}
+              isActive={isActive}
+              background={backgroundColor}
+              {...props}
+          >
+              {icon(iconColor)}
+          </ Button2>
+      }
+    />
   );
 };
 
 MarkButton.propTypes = {
-  format: PropTypes.string,
-  icon: PropTypes.string
+  icon: PropTypes.func,
+  type: PropTypes.string,
+  label: PropTypes.string,
 };
 
 export default MarkButton;
