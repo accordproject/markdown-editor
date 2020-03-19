d003d26fab39d73c57943004a25ece869c13c9c6
irmerk <jolenelanglinais@gmail.com>
2020-03-19 16:25:17 -0400

    refactor(FormattingToolbar): swap mark and block style buttons - I297
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/RichTextEditor/FormattingToolbar/index.js
+++ src/RichTextEditor/FormattingToolbar/index.js
@@ -3,14 +3,16 @@ import { InsertImageButton } from '../components/withImages';
 import ToolbarMenu from './ToolbarMenu';
 import BlockButton from './BlockButton';
 import MarkButton from './MarkButton';
+import StyleDropdown from './StyleDropdown';
+
+import { bold, italic, code } from '../components/icons';
 
 const FormattingToolbar = () => (
   <ToolbarMenu>
-    <MarkButton format="bold" icon="format_bold" />
-    <MarkButton format="italic" icon="format_italic" />
-    <MarkButton format="code" icon="code" />
-    <BlockButton format="heading_one" icon="looks_one" />
-    <BlockButton format="heading_two" icon="looks_two" />
+    <StyleDropdown />
+    <MarkButton {...bold} />
+    <MarkButton {...italic} />
+    <MarkButton {...code} />
     <BlockButton format="block_quote" icon="format_quote" />
     <BlockButton format="ol_list" icon="format_list_numbered" />
     <BlockButton format="ul_list" icon="format_list_bulleted" />
