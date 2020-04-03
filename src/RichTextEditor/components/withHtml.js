02920caf6e1669119d2d79244ead32f39086e840
irmerk <jolenelanglinais@gmail.com>
2020-04-03 12:50:45 -0400

    pause wip
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/RichTextEditor/components/withHtml.js
+++ src/RichTextEditor/components/withHtml.js
@@ -1,21 +1,42 @@
 import { Transforms } from 'slate';
-import { SlateTransformer } from '@accordproject/markdown-slate';
+import { CiceroMarkTransformer } from '@accordproject/markdown-cicero';
 import { HtmlTransformer } from '@accordproject/markdown-html';
+import { SlateTransformer } from '@accordproject/markdown-slate';
 
 /* eslint no-param-reassign: 0 */
 export const withHtml = (editor) => {
   const { insertData } = editor;
 
   editor.insertData = (data) => {
-    const html = data.getData('text/html');
-
-    if (html) {
+    const HTML_DOM = data.getData('text/html');
+    const PLAIN_DOM = data.getData('text/plain');
+    console.log('HTML_DOM???', HTML_DOM);
+    console.log('PLAIN_DOM>>>', PLAIN_DOM);
+    if (HTML_DOM) {
+      console.log('HTML_DOM FIRED');
       try {
         const htmlTransformer = new HtmlTransformer();
         const slateTransformer = new SlateTransformer();
-        const dom = htmlTransformer.toCiceroMark(html);
-        const slate = slateTransformer.fromCiceroMark(dom);
-        Transforms.insertFragment(editor, slate.document.children);
+        // const CICERO_MARK_DOM = htmlTransformer.toCiceroMark(HTML_DOM);
+        const SLATE_DOM = slateTransformer
+          .fromCiceroMark(htmlTransformer.toCiceroMark(HTML_DOM));
+        Transforms.insertFragment(editor, SLATE_DOM.document.children);
+      } catch (err) {
+        console.error(err);
+      }
+      return;
+    }
+    if (PLAIN_DOM) {
+      console.log('PLAIN_DOM FIRED');
+      try {
+        // const htmlTransformer = new HtmlTransformer();
+        const slateTransformer = new SlateTransformer();
+        const ciceroMarkTransformer = new CiceroMarkTransformer();
+        const CICERO_MARK_DOM = ciceroMarkTransformer.fromMarkdown(PLAIN_DOM);
+        console.log('CICERO_MARK_DOM FIRED', CICERO_MARK_DOM);
+        const SLATE_DOM = slateTransformer.fromCiceroMark(CICERO_MARK_DOM);
+        console.log('SLATE_DOM FIRED', SLATE_DOM);
+        Transforms.insertFragment(editor, SLATE_DOM.document.children);
       } catch (err) {
         console.error(err);
       }
