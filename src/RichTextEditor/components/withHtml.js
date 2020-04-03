2a6c6e056825c9007489e87750d03dc6a2a54e37
irmerk <jolenelanglinais@gmail.com>
2020-04-03 14:27:41 -0400

    feat(RichTextEditor): handle plaintext paste - I297
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/RichTextEditor/components/withHtml.js
+++ src/RichTextEditor/components/withHtml.js
@@ -10,32 +10,18 @@ export const withHtml = (editor) => {
   editor.insertData = (data) => {
     const HTML_DOM = data.getData('text/html');
     const PLAIN_DOM = data.getData('text/plain');
-    console.log('HTML_DOM???', HTML_DOM);
-    console.log('PLAIN_DOM>>>', PLAIN_DOM);
-    if (HTML_DOM) {
-      console.log('HTML_DOM FIRED');
+    if (HTML_DOM || PLAIN_DOM) {
       try {
         const htmlTransformer = new HtmlTransformer();
         const slateTransformer = new SlateTransformer();
-        // const CICERO_MARK_DOM = htmlTransformer.toCiceroMark(HTML_DOM);
-        const SLATE_DOM = slateTransformer
-          .fromCiceroMark(htmlTransformer.toCiceroMark(HTML_DOM));
-        Transforms.insertFragment(editor, SLATE_DOM.document.children);
-      } catch (err) {
-        console.error(err);
-      }
-      return;
-    }
-    if (PLAIN_DOM) {
-      console.log('PLAIN_DOM FIRED');
-      try {
-        // const htmlTransformer = new HtmlTransformer();
-        const slateTransformer = new SlateTransformer();
         const ciceroMarkTransformer = new CiceroMarkTransformer();
-        const CICERO_MARK_DOM = ciceroMarkTransformer.fromMarkdown(PLAIN_DOM, 'json');
-        console.log('CICERO_MARK_DOM FIRED', CICERO_MARK_DOM);
-        const SLATE_DOM = slateTransformer.fromCiceroMark(CICERO_MARK_DOM);
-        console.log('SLATE_DOM FIRED', SLATE_DOM);
+
+        const SLATE_DOM = HTML_DOM
+          ? slateTransformer
+            .fromCiceroMark(htmlTransformer.toCiceroMark(HTML_DOM))
+          : slateTransformer
+            .fromCiceroMark(ciceroMarkTransformer.fromMarkdown(PLAIN_DOM));
+
         Transforms.insertFragment(editor, SLATE_DOM.document.children);
       } catch (err) {
         console.error(err);
