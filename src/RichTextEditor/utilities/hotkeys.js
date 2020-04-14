e4532e3b26e9820e4275c03327ebf92f70dcbbd1
irmerk <jolenelanglinais@gmail.com>
2020-04-14 16:32:35 -0400

    feat(RichTextEditor): break from empty block on enter - I321
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/RichTextEditor/utilities/hotkeys.js
+++ src/RichTextEditor/utilities/hotkeys.js
@@ -1,33 +1,37 @@
-import * as SCHEMA from './schema';
+import {
+  PARAGRAPH, LINK, IMAGE, H1, H2, H3, H4, H5, H6, HR,
+  CODE_BLOCK, HTML_BLOCK, BLOCK_QUOTE, UL_LIST, OL_LIST, LIST_ITEM,
+  HTML_INLINE, SOFTBREAK, LINEBREAK, MARK_BOLD, MARK_ITALIC, MARK_CODE
+} from './schema';
 
 const HOTKEYS = {
   'mod+b': {
     type: 'mark',
-    code: SCHEMA.MARK_BOLD,
+    code: MARK_BOLD,
   },
   'mod+i': {
     type: 'mark',
-    code: SCHEMA.MARK_ITALIC,
+    code: MARK_ITALIC,
   },
   'mod+shift+9': {
     type: 'mark',
-    code: SCHEMA.MARK_CODE,
+    code: MARK_CODE,
   },
   'mod+shift+7': {
     type: 'block',
-    code: SCHEMA.OL_LIST,
+    code: OL_LIST,
   },
   'mod+shift+8': {
     type: 'block',
-    code: SCHEMA.UL_LIST,
+    code: UL_LIST,
   },
   'mod+shift+.': {
     type: 'block',
-    code: SCHEMA.BLOCK_QUOTE,
+    code: BLOCK_QUOTE,
   },
   'mod+shift+g': {
     type: 'image',
-    code: SCHEMA.IMAGE,
+    code: IMAGE,
   },
   'mod+z': {
     type: 'special',
@@ -39,6 +43,15 @@ const HOTKEYS = {
   }
 };
 
+export const ENTER_BREAKS = {
+  [BLOCK_QUOTE]: true,
+  [OL_LIST]: true,
+  [UL_LIST]: true,
+};
+
 export const formattingHotKeys = ['mod+b', 'mod+i', 'mod+shift+7', 'mod+shift+8', 'mod+shift+9', 'mod+shift+.', 'mod+shift+g'];
+
+export const ENTER = 'enter';
+export const ENTER_SHIFT = 'shift+enter';
 
 export default HOTKEYS;
